2f8ec7bde4af7e3c43dc4b99526a60bf9b710582
jarfy <jarfy8@hotmail.com>
2016-12-09 11:33:32 +0100

    Restructuring files, creation of WebAPP files

---
+++ Kung-Persistence/src/main/java/sing/proyectosi/Production.java
@@ -0,0 +1,122 @@
+package sing.proyectosi;
+
+import java.util.Date;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+
+@Entity
+public class Production {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idProduction;	
+	private String title;
+	private Date year;
+	private String genre;
+	private String released;
+	private int runtime;
+	private String plot;
+	private String languaje;
+	//type of product
+	private String type;
+	private int totalSeason;
+	private int episode;
+	
+		
+	public int getIdProduction() {
+		return idProduction;
+	}
+	
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getYear() {
+		return year;
+	}
+
+	public void setYear(Date year) {
+		this.year = year;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getRelease() {
+		return released;
+	}
+
+	public void setRelease(String release) {
+		this.released = release;
+	}
+
+	public int getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(int runtime) {
+		this.runtime = runtime;
+	}
+
+	public String getPlot() {
+		return plot;
+	}
+
+	public void setPlot(String plot) {
+		this.plot = plot;
+	}
+
+	public String getLanguaje() {
+		return languaje;
+	}
+
+	public void setLanguaje(String languaje) {
+		this.languaje = languaje;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getTotalSeason() {
+		return totalSeason;
+	}
+
+	public void setTotalSeason(int totalSeason) {
+		this.totalSeason = totalSeason;
+	}
+
+	public int getEpisode() {
+		return episode;
+	}
+
+	public void setEpisode(int episode) {
+		this.episode = episode;
+	}
+
+		
+	
+	
+}
